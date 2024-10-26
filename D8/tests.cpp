be04857c83f9cbe2785c60908f89fcc272fe8594
af2167 <af2167@gmail.com>
2024-10-25 23:07:46 -0500

    adding catch2 testing, but the tests do not run yet

---
+++ D8/tests.cpp
@@ -0,0 +1,8 @@
+import formatting;
+
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Formatted die with no set bonus", "[format_die]") {
+  using namespace std::string_literals;
+  REQUIRE(format::format_die(20, 1) == "(1)"s);
+}
